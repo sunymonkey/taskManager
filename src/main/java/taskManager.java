d2233b6acebda61dd45d9afaa6eb44a1167afa67
piotr <mr.piotr.ratajczyk@gmail.com>
2021-05-21 09:04:47 +0200

    Poprawienie metod

--- src/main/java/taskManager.java
+++ src/main/java/taskManager.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class taskManager {
     public static void main(String[] args) {
@@ -69,29 +66,50 @@ public class taskManager {
     }
 
     private static String[][] remove(String[][] tabs) {
-        System.out.println("Please select number to remove.");
         Scanner scanner = new Scanner(System.in);
-        int index = scanner.nextInt();
+        System.out.println("Please select number to remove.");
+        int index = 0;
+        while (true) {
+            try {
+                index = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.next();
+                index = -1;
+            }
+            if(index < 0 || index > tabs.length){
+                System.out.println("Incorrect argument passed. Please give number greater or equal 0");
+            } else {
+                break;
+            }
+        }
         tabs = ArrayUtils.remove(tabs, index);
+
         return tabs;
     }
 
     private static String[][] readFile(String fileName) {
         Path path = Paths.get(fileName);
+
         //czytam plik
         String[][] tabs = new String[0][3];
-        try {
-            for (String line: Files.readAllLines(path)) {
-                tabs = Arrays.copyOf(tabs, tabs.length+1);
-                tabs[tabs.length-1] = new String[3];
-                String[] parts = line.split(", ");
-                tabs[tabs.length-1][0] = parts[0];
-                tabs[tabs.length-1][1] = parts[1];
-                tabs[tabs.length-1][2] = parts[2];
+
+        if (Files.exists(path)) {
+            try {
+                for (String line: Files.readAllLines(path)) {
+                    tabs = Arrays.copyOf(tabs, tabs.length+1);
+                    tabs[tabs.length-1] = new String[3];
+                    String[] parts = line.split(", ");
+                    tabs[tabs.length-1][0] = parts[0];
+                    tabs[tabs.length-1][1] = parts[1];
+                    tabs[tabs.length-1][2] = parts[2];
+                }
+            } catch (IOException e) {
+                System.out.println("Problem z plikiem " + e.getMessage());
             }
-        } catch (IOException e) {
-            System.out.println("Problem z plikiem " + e.getMessage());
+        } else {
+            System.out.println(ConsoleColors.RED + "Plik nie istnieje/brak zadań do wykonania!" + ConsoleColors.RESET);
         }
+
         return tabs;
     }
 
@@ -112,13 +130,15 @@ public class taskManager {
         return tabs;
     }
 
-
     private static void list(String[][] tabs) {
         // wyswietlam liste:
-        for (int i = 0; i < tabs.length; i++) {
-            System.out.println(i + ":  " + tabs[i][0] + "  " + tabs[i][1] + "  " + tabs[i][2]);
+        if (tabs.length == 0) {
+            System.out.println(ConsoleColors.RED + "Brak zadań do wykonania!" + ConsoleColors.RESET);
+        } else {
+            for (int i = 0; i < tabs.length; i++) {
+                System.out.println(i + ":  " + tabs[i][0] + "  " + tabs[i][1] + "  " + tabs[i][2]);
+            }
         }
     }
-
 
 }
